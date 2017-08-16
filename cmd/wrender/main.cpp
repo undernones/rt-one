5cc093a184fd0aadf4f507f302f46dc2c6b9ed68
Stephen Ward <stephen@wardfam.com>
2017-08-15 18:14:49 -0700

    Check for and handle error codes.

--- cmd/wrender/main.cpp
+++ cmd/wrender/main.cpp
@@ -27,16 +27,44 @@ private:
     RTCDevice mDevice;
 };
 
+std::string
+EmbreeErrorToString(RTCError error)
+{
+    switch (error) {
+        case RTC_NO_ERROR:
+            return "No error";
+
+        case RTC_UNKNOWN_ERROR:
+            return "Unknown";
+
+        case RTC_INVALID_ARGUMENT:
+            return "Invalid argument";
+
+        case RTC_INVALID_OPERATION:
+            return "Invalid operation";
+
+        case RTC_OUT_OF_MEMORY:
+            return "Out of memory";
+
+        case RTC_UNSUPPORTED_CPU:
+            return "Unsupported CPU";
+
+        case RTC_CANCELLED:
+            return "Cancelled";
+    }
+}
+
 int
 main(int argc, const char * argv[])
 {
     _MM_SET_FLUSH_ZERO_MODE(_MM_FLUSH_ZERO_ON);
     _MM_SET_DENORMALS_ZERO_MODE(_MM_DENORMALS_ZERO_ON);
-    
+
     auto device = DeviceHandle();
     if (!device) {
-        std::cerr << "Failed to initialize device" << std::endl;
-        return -1;
+        auto error = rtcDeviceGetError(NULL);
+        std::cerr << "Failed to initialize device: " << EmbreeErrorToString(error) << std::endl;
+        return error;
     }
 
     std::cout << "Hello, World!\n";
