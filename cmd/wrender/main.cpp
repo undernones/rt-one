bf0bbad665e28bb1cab7e0f83bb2be1df7e78342
Stephen Ward <stephen@wardfam.com>
2017-08-15 18:08:36 -0700

    Ensure proper Embree behavior by setting modes.

--- cmd/wrender/main.cpp
+++ cmd/wrender/main.cpp
@@ -5,6 +5,8 @@
 #include <iostream>
 #include <embree2/rtcore.h>
 #include <embree2/rtcore_ray.h>
+#include <xmmintrin.h>
+#include <pmmintrin.h>
 
 class DeviceHandle
 {
@@ -28,12 +30,15 @@ private:
 int
 main(int argc, const char * argv[])
 {
+    _MM_SET_FLUSH_ZERO_MODE(_MM_FLUSH_ZERO_ON);
+    _MM_SET_DENORMALS_ZERO_MODE(_MM_DENORMALS_ZERO_ON);
+    
     auto device = DeviceHandle();
     if (!device) {
         std::cerr << "Failed to initialize device" << std::endl;
         return -1;
     }
-    
+
     std::cout << "Hello, World!\n";
     return 0;
 }
