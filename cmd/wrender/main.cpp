404c45d400cd8afa635f764b252dcd014612aec1
Stephen Ward <stephen@wardfam.com>
2017-08-15 17:35:32 -0700

    Bare bones main file.

---
+++ cmd/wrender/main.cpp
@@ -0,0 +1,13 @@
+//
+//  Copyright © 2017 Undernones. All rights reserved.
+//
+
+#include <iostream>
+
+int
+main(int argc, const char * argv[])
+{
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
