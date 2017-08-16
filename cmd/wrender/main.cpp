968a2b71be5b7a40d60789379d2e8e90f36fac42
Stephen Ward <stephen@wardfam.com>
2017-08-15 18:07:07 -0700

    Wrap device objects up in a DeviceHandle that will automatically clean up resources for us.

--- cmd/wrender/main.cpp
+++ cmd/wrender/main.cpp
@@ -6,11 +6,34 @@
 #include <embree2/rtcore.h>
 #include <embree2/rtcore_ray.h>
 
+class DeviceHandle
+{
+public:
+    DeviceHandle(const char* cfg = NULL) : DeviceHandle(rtcNewDevice(cfg)) {}
+    DeviceHandle(RTCDevice device) : mDevice(device) {}
+
+    ~DeviceHandle() {
+        if (mDevice != nullptr) {
+            rtcDeleteDevice(mDevice);
+        }
+    }
+
+    operator RTCDevice() const { return mDevice; }
+    operator bool() const { return mDevice != nullptr; }
+
+private:
+    RTCDevice mDevice;
+};
+
 int
 main(int argc, const char * argv[])
 {
-    RTCDevice device = rtcNewDevice(NULL);
-    rtcDeleteDevice(device);
+    auto device = DeviceHandle();
+    if (!device) {
+        std::cerr << "Failed to initialize device" << std::endl;
+        return -1;
+    }
+    
     std::cout << "Hello, World!\n";
     return 0;
 }
