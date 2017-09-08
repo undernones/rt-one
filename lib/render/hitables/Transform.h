e8e6aa3ecabbe50994682fd7f549f2a087500ba5
Stephen Ward <stephen@wardfam.com>
2017-09-08 08:21:04 -0700

    Start adding a generic Transform Hitable.

---
+++ lib/render/hitables/Transform.h
@@ -0,0 +1,10 @@
+//
+// Copyright © 2017 Undernones. All rights reserved.
+//
+
+#ifndef RENDER_TRANSFORM_H
+#define RENDER_TRANSFORM_H
+
+#include <render/Hitable.h>
+
+#endif // RENDER_TRANSFORM_H
