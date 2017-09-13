c867353264003ae3c2e911add3f46cbddfe34fec
Stephen Ward <stephen@wardfam.com>
2017-09-13 10:57:17 -0700

    Correct the Metal material by normalizing the reflected vector.

--- lib/render/materials/Metal.cpp
+++ lib/render/materials/Metal.cpp
@@ -4,33 +4,23 @@
 
 #include "Metal.h"
 
+#include <algorithm>
 #include <geom/Utils.h>
 
-namespace
-{
-
-template <typename T>
-inline T
-clamp(T const& value, T const& min, T const& max)
-{
-    return std::max(min, std::min(value, max));
-}
-
-}
 namespace render
 {
 
 Metal::Metal(const geom::Vec3& albedo, float fuzziness)
     : Material()
     , mAlbedo(albedo)
-    , mFuzziness(clamp(fuzziness, 0.f, 1.f))
+    , mFuzziness(std::clamp(fuzziness, 0.f, 1.f))
 {
 }
 
 bool
 Metal::scatter(const Ray& rayIn, geom::Vec3& attenuation, Ray& scattered) const
 {
-    auto reflected = geom::reflect(rayIn.direction, rayIn.normal);
+    auto reflected = geom::reflect(rayIn.direction, rayIn.normal).normalized();
 
     const auto hitPoint = rayIn.pointAt(rayIn.tfar);
     scattered = Ray(hitPoint, reflected + fuzziness() * geom::randomInUnitSphere(), rayIn.time);
