fbd6d80aa79a12be459c90ff77f4964b3b09fd97
Stephen Ward <stephen@wardfam.com>
2017-09-18 17:34:54 -0700

    Update the teapot scene with many instances.

--- lib/render/scenes/TeapotScene.cpp
+++ lib/render/scenes/TeapotScene.cpp
@@ -18,6 +18,7 @@
 #include "RenderableList.h"
 #include "Rotate.h"
 #include "Sphere.h"
+#include "Translate.h"
 
 using namespace std;
 
@@ -38,16 +39,26 @@ TeapotScene::TeapotScene(int width, int height)
     list.emplace_back(make_shared<Rotate>(geom::Vec3(1, 0, 0), -90, mesh));
     list.emplace_back(make_shared<Rectangle>(Plane::XZ, -600, 600, -600, 600, 0.5, metalMaterial));
 
+    auto otherPotMaterial = make_shared<Lambertian>(make_shared<ConstantTexture>(geom::Vec3(0, 0.2, 0.7)));
+    auto otherMesh = make_shared<Rotate>(geom::Vec3(1, 0, 0), -90, make_shared<Mesh>(triMesh, otherPotMaterial));
+    for (auto i = -10; i <= 10; ++i) {
+        for (auto j = -10; j <= 10; ++j) {
+            if (i == 0 && j == 0) continue;
+            auto position = geom::Vec3(40 * i, 0, 40 * j);
+            list.emplace_back(make_shared<Translate>(position, otherMesh));
+        }
+    }
+
     // Make a light and put a bit of a frame around it
     const auto LIGHT_LEFT = -12;
     const auto LIGHT_RIGHT = 12;
     const auto LIGHT_BACK = -12;
     const auto LIGHT_FRONT = 12;
-    const auto LIGHT_TOP = 55;
-    const auto LIGHT_BOX_HEIGHT = 25;
+    const auto LIGHT_TOP = 95;
+    const auto LIGHT_BOX_HEIGHT = 15;
     auto lightBoxTexture = make_shared<ConstantTexture>(geom::Vec3(0.01f));
     auto lightBoxMaterial = make_shared<Lambertian>(lightBoxTexture);
-    auto light = make_shared<DiffuseLight>(make_shared<ConstantTexture>(geom::Vec3(14.f)));
+    auto light = make_shared<DiffuseLight>(make_shared<ConstantTexture>(geom::Vec3(21.f)));
     list.emplace_back(make_shared<Rectangle>(Plane::XZ, LIGHT_LEFT, LIGHT_RIGHT, LIGHT_BACK, LIGHT_FRONT, LIGHT_TOP, light));
     list.emplace_back(make_shared<Rectangle>(Plane::XY, LIGHT_LEFT, LIGHT_RIGHT, LIGHT_TOP-LIGHT_BOX_HEIGHT, LIGHT_TOP, LIGHT_BACK, lightBoxMaterial));
     list.emplace_back(make_shared<Rectangle>(Plane::YZ, LIGHT_TOP-LIGHT_BOX_HEIGHT, LIGHT_TOP, LIGHT_BACK, LIGHT_FRONT, LIGHT_LEFT, lightBoxMaterial));
@@ -60,14 +71,14 @@ TeapotScene::TeapotScene(int width, int height)
     mRoot = std::make_shared<RenderableList>(list);
     commit();
 
-    auto eye = geom::Vec3(0, 10, -60);
-    auto lookAt = geom::Vec3(0, 10, 0);
+    auto eye = geom::Vec3(0, 90, -130);
+    auto lookAt = geom::Vec3(0, 0, -10);
     auto up = geom::Vec3(0, 1, 0);
     auto focusDistance = (eye - lookAt).length();
     auto aperture = 0.f;
-    mCamera = Camera(eye, lookAt, up, 45, width, height, aperture, focusDistance, 0, 1);
+    mCamera = Camera(eye, lookAt, up, 55, width, height, aperture, focusDistance, 0, 1);
 
-    mBgIntensity = 0.01f;
+    mBgIntensity = 0.5f;
 }
 
 }
